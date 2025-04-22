b39de1471633cf9a7cec0e34c90c1fa7c42d9d80
kota-okano <kota.okano@proaxia-consulting.co.jp>
2025-04-22 10:25:20 +0900

    パッケージ更新

--- Package.swift
+++ Package.swift
@@ -14,10 +14,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MyPackage"),
+        .target(name: "MyPackage"),
         .testTarget(
-            name: "MyPackageTests",
+            name: "LanguagesTests",
+            dependencies: ["MyPackage"]
+        ),
+        .testTarget(
+            name: "SearchTests",
             dependencies: ["MyPackage"]
         ),
     ]
